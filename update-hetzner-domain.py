d89808adde01a64e9ef9ea73d4426367d337981b
Kamil Essekkat <kamil@essekkat.pl>
2023-07-23 12:13:15 +0200

    Add more logging

--- update-hetzner-domain.py
+++ update-hetzner-domain.py
@@ -6,12 +6,13 @@ from os import environ
 from typing import Literal
 
 import requests
+from loguru import logger
 from pydantic import BaseModel
 
 API_KEY = environ["HETZNER_DNS_API_KEY"]
 DOMAIN = environ["DYNAMIC_DOMAIN"]
 WILDCARD = "*"
-INTERVAL = 15 * 60  # seconds
+INTERVAL = 15  # * 60  # seconds
 READ_ONLY = False
 
 
@@ -69,13 +70,15 @@ def create_record(record: Record):
             "Content-Type": "application/json",
             "Auth-API-Token": API_KEY,
         },
-        data=json.dumps({
-            "value": record.value,
-            "ttl": record.ttl,
-            "type": record.type,
-            "name": record.name,
-            "zone_id": record.zone.id,
-        })
+        data=json.dumps(
+            {
+                "value": record.value,
+                "ttl": record.ttl,
+                "type": record.type,
+                "name": record.name,
+                "zone_id": record.zone.id,
+            }
+        ),
     )
 
 
@@ -110,31 +113,26 @@ def get_my_ip():
 
 def main(read_only=False):
     for zone in get_zones():
-        print(f"Zone {zone.id} name: {zone.name}")
+        logger.info(f"Zone {zone.id} name: {zone.name}")
         can_update_record = None
         for record in get_records(zone):
-            print(f"\t{record.name} {record.ttl} IN {record.type} {record.value}")
+            logger.info(f"\t{record.name} {record.ttl} IN {record.type} {record.value}")
             if zone.name == DOMAIN and record.name == WILDCARD:
                 can_update_record = record
         if zone.name == DOMAIN:
             if read_only:
-                print(f"R/O: existing record: {can_update_record} ")
+                logger.info(f"R/O: existing record: {can_update_record} ")
                 return
             my_ip = get_my_ip()
             if can_update_record:
                 update_record(can_update_record, my_ip)
             else:
-                create_record(
-                    Record(
-                        zone=zone,
-                        name=WILDCARD,
-                        type="A",
-                        value=my_ip
-                    )
-                )
+                create_record(Record(zone=zone, name=WILDCARD, type="A", value=my_ip))
 
 
 if __name__ == "__main__":
+    logger.info("Starting script")
     while True:
-        time.sleep(INTERVAL)
         main(read_only=READ_ONLY)
+        logger.info(f"Sleep for {INTERVAL} seconds")
+        time.sleep(INTERVAL)
